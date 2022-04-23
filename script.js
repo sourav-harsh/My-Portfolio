8d15510f66ac8a315a3fcefeda0dfa21c9ac76de
Sourav Kumar <74768346+sourav-harsh@users.noreply.github.com>
2022-04-23 19:01:57 +0530

    Potfolio

---
+++ script.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY>20)
+        {
+            $('.navbar').addClass("sticky");
+        }
+        else
+        {
+            $('.navbar').removeClass("sticky");
+        }
+        if(this.scrollY>500)
+        {
+            $('.scroll-up-btn').addClass("show");
+        }
+        else
+        {
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+    // slide up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0});
+    });
+    // toggle menu/navbar script
+    $('menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    // owl carousel script
+$('.carousel').owlCarousel({
+   margin:20,
+   loop:true,
+   autoplayTimeOut: 2000,
+   autoplayHoverPause:true,
+   responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        600:{
+            items:2,
+            nav:false
+        },
+        1000:{
+            items:3,
+            nav:false
+        }
+   }
+});
+});
+
+// typing Animation script
+var typed = new Typed(".typing",{
+    strings:["Youtuber","Developer","Designer","DSA lover","Coder","Gamer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
+var typed = new Typed(".typing-1",{
+    strings:["DSA lover","Coder","Gamer","Youtuber","Developer","Designer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
