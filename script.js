85d51018917d292a891d08eb5e42eac58e1288ac
Sourav Kumar <74768346+sourav-harsh@users.noreply.github.com>
2022-07-22 21:24:05 +0530

    Adding animations

--- script.js
+++ script.js
@@ -63,6 +63,10 @@ var typed = new Typed(".typing-1",{
     loop:true
 });
 
+AOS.init({
+    duration: 1200,
+  })
+ 
 
 
 // Project script
@@ -101,3 +105,50 @@ const person={
     }
 }
 person.about();
+
+
+
+
+// ##############################Animation###############################
+// gsap.registerPlugin(ScrollTrigger);
+// // REVEAL //
+// gsap.utils.toArray(".revealUp").forEach(function (elem) {
+//   ScrollTrigger.create({
+//     trigger: elem,
+//     start: "top 80%",
+//     end: "bottom 20%",
+//     markers: true,
+//     onEnter: function () {
+//       gsap.fromTo(
+//         elem,
+//         { y: 100, autoAlpha: 0 },
+//         {
+//           duration: 1.25,
+//           y: 0,
+//           autoAlpha: 1,
+//           ease: "back",
+//           overwrite: "auto"
+//         }
+//       );
+//     },
+//     onLeave: function () {
+//       gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+//     },
+//     onEnterBack: function () {
+//       gsap.fromTo(
+//         elem,
+//         { y: -100, autoAlpha: 0 },
+//         {
+//           duration: 1.25,
+//           y: 0,
+//           autoAlpha: 1,
+//           ease: "back",
+//           overwrite: "auto"
+//         }
+//       );
+//     },
+//     onLeaveBack: function () {
+//       gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+//     }
+//   });
+// });
